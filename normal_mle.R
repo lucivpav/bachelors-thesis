cfe669a5875a060627866f08e896599fa6460f14
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2018-05-08 22:18:32 +0200

    improve param estimation

--- normal_mle.R
+++ normal_mle.R
@@ -3,5 +3,5 @@ pdf("normal_mle.pdf")
 count <- 100;
 data <- rnorm(count, mean = 40, sd = 32);
 est <- fitdistr(data, densfun="normal")$estimate;
-hist(data, freq = F, breaks = 10);
+hist(data, freq = F, breaks = 10, ylab="Density", xlab="Value", main="");
 curve(dnorm(x, est[1], est[2]), col="green", lwd=2, add=T);
